8c97e186c8708b9589b32cff3668a82caff7ed30
Jeff Casimir <jeff@casimircreative.com>
2013-04-23 09:51:32 -0600

    DOSE assignment collector

---
+++ projects/daughter_of_store_engine.rb
@@ -0,0 +1,16 @@
+def project
+  {
+    :gates   => {:repo  => 'git://github.com/x/y.git',
+                 :owner => 'a@b.com'},
+    :ellison => {:repo  => 'git://github.com/x/y.git',
+                 :owner => 'a@b.com'},
+    :woz     => {:repo  => 'git://github.com/x/y.git',
+                 :owner => 'a@b.com'},
+    :jobs    => {:repo  => 'git://github.com/x/y.git',
+                 :owner => 'a@b.com'},
+    :ballmer => {:repo  => 'git://github.com/x/y.git',
+                 :owner => 'a@b.com'},
+    :allen   => {:repo  => 'git://github.com/x/y.git',
+                 :owner => 'a@b.com'},
+  }
+end
