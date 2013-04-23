10a7ae355eb81b27594bab1ce431814d14ba118c
John Maddux <JEMaddux@gmail.com>
2013-04-23 10:15:16 -0600

    Updated

--- projects/daughter_of_store_engine.rb
+++ projects/daughter_of_store_engine.rb
@@ -5,8 +5,8 @@ def project
   {
     :gates   => {:repo  => 'git://github.com/x/y.git',
                  :collaborators => ['a@b.com', 'c@d.com']},
-    :ellison => {:repo  => 'git://github.com/x/y.git',
-                 :collaborators => ['a@b.com', 'c@d.com']},
+    :ellison => {:repo  => 'git@github.com:philbattos/daughter_of_store_engine.git',
+                 :collaborators => ['jemaddux@gmail.com', 'pnblackwell@gmail.com', 'philbattos@gmail.com', 'blair81@gmail.com']},
     :woz     => {:repo  => 'git://github.com/x/y.git',
                  :collaborators => ['a@b.com', 'c@d.com']},
     :jobs    => {:repo  => 'git://github.com/x/y.git',
