a4f71e3175264e5e160a818de3227d23436b8c8a
Jeff Casimir <jeff@jumpstartlab.com>
2013-05-19 14:19:16 -0600

    Adding Tuneline repo without collaborators. (#105)

--- projects/feed_engine.rb
+++ projects/feed_engine.rb
@@ -11,8 +11,8 @@ def project
                    :collaborators => ['Logan Sears', 'Chelsea Komlo', 'Daniel Mee']},
     :traveline => {:repo  => 'https://github.com/raphweiner/feed_engine',
                    :collaborators => ['raphael.weiner@gmail.com', 'e.b.drummond@gmail.com']},
-    :tuneline  => {:repo  => '',
-                   :collaborators => []},
+    :tuneline  => {:repo  => 'https://github.com/aimzatron/feed_engine',
+                   :collaborators => []}
     :runline   => {:repo  => 'git@github.com:bradpsheehan/feed_engine.git',
                    :collaborators => ['bradpsheehan@gmail.com', 'lalalainexd@gmail.com.com', 'jemaddux@gmail.com', 'kareem@getuserwise.com']}
   }
