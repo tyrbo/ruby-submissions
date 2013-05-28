a2dd0bc254344c8f87f07217aaa8ef1e549c3daa
Danny Garcia <dannygarcia.me@gmail.com>
2013-05-28 14:32:07 -0600

    Danny's badass individual project info.

---
+++ projects/individual_projects/garcia_danny.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "Garcia",
+  :first_name => "Danny",
+  :project_name => "EconoMapper",
+  :project_description => "Integrates with Data.gov's api to provide a data visualization of economic activity (GDP) in the U.S using D3",
+  :technologies => ['D3'],
+  :integrations => ['data.gov']
+  :repo => 'TBA',
+  :heroku_account => 'dannygarcia.me@gmail.com'
+}
