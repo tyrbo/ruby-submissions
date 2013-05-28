8c37d0542f4d608e056f88121af9ed0382ca91ee
Blair Anderson <blair81@gmail.com>
2013-05-28 14:47:08 -0600

    submitting solo idea

---
+++ projects/individual_projects/anderson_blair.rb
@@ -0,0 +1,14 @@
+# 1) Create a file like this with your own lastname_firstname.rb
+# 2) Fill in the details
+# 3) Submit a pull request
+
+{
+  :last_name => "Anderson",
+  :first_name => "Blair",
+  :project_name => "Hot or Not for Youtube-Music/Soundcloud",
+  :project_description => "Generating playlists that mix youtube music and soundcloud tracks. I'll use websockets to push new updates to the user.",
+  :technologies => ['Websockets'],
+  :integrations => ['youtube', 'soundcloud']
+  :repo => 'http://github.com/blairanderson/solo-project',
+  :heroku_account => 'blair81@gmail.com'
+}
