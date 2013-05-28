b5f96c32d5b42ba5a75f20c6595376639b3878f1
Danny Garcia <dannygarcia.me@gmail.com>
2013-05-28 15:20:20 -0600

    Detailed description of individual project

--- projects/individual_projects/garcia_danny.rb
+++ projects/individual_projects/garcia_danny.rb
@@ -2,7 +2,7 @@
   :last_name => "Garcia",
   :first_name => "Danny",
   :project_name => "EconoMapper",
-  :project_description => "Integrates with Data.gov's api to provide a data visualization of economic activity (GDP) in the U.S using D3",
+  :project_description => "EconoMapper provides a data visualization of Economic Activity in the U.S. Allowing users to get a visual representation of various economic indicators state by state and the country as a whole. With this information for example, you can draw correlations between GDP and energy consumption in the U.S. or conduct research on business activities in industries and communities during the past few years.",
   :technologies => ['D3'],
   :integrations => ['data.gov']
   :repo => 'TBA',
