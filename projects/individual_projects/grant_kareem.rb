dcaa1b3d47763f11cba9413d8470a12a204affdc
Kareem Grant <kareem@getuserwise.com>
2013-05-28 14:03:00 -0600

    submission of project proposal

---
+++ projects/individual_projects/grant_kareem.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "Grant",
+  :first_name => "Kareem",
+  :project_name => "shot_visualizer",
+  :project_description => "ShotVisualizer takes NBA shooting location data and provide users with a visual way to analyze a team's or individual's shooting performance from specific areas of the court. Visual shot location data has the power to reveal the 'hidden' statistics of the game that are not detailed in traditional box scores. ShotVisualizer has a variety of uses which include scouting, player development, player acquisition and research."
+  :technologies => ['MongoDB'],
+  :integrations => ['Sports(NBA.com, etc.)']
+  :repo => 'http://github.com/kareemgrant/shot_visualizer',
+  :heroku_account => 'kareem@getuserwise.com'
+}
