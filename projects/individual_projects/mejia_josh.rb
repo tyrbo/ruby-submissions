1950878563c26234391dc8ad958cd3ad1307245b
Josh Mejia <joshua.mejia@gmail.com>
2013-05-28 13:47:20 -0600

    added personal project info

---
+++ projects/individual_projects/mejia_josh.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "mejia",
+  :first_name => "josh",
+  :project_name => "sparklr",
+  :project_description => "'Do you have a lot of ideas but no clue how to organize them? Or maybe ideas come to you and by the time you have a chance to record them, you've forgotten? Enter the Spark File. As Alex Hillman explains, this tool doesn't just capture half-baked ideas—it helps you turn small concepts into great things.' -lifehacker. Sparklr is an online app that parses spark files saved in txt format and saves individual lines as items in a database. It integrates with Dropbox to allow you to continue to add content even if you aren't currently online.",
+  :technologies => ['riak'],
+  :integrations => ['dropbox', 'sendgrid']
+  :repo => 'http://github.com/jmejia/sparklr',
+  :heroku_account => 'joshua.mejia@gmail.com'
+}
