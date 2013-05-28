7115552052ad36a8839a2f635313479bae67c038
Chelsea Komlo <chelsea.komlo@gmail.com>
2013-05-28 13:53:48 -0600

    chelsea komlo

---
+++ projects/individual_projects/komlo_chelsea.rb
@@ -0,0 +1,14 @@
+# 1) Create a file like this with your own lastname_firstname.rb
+# 2) Fill in the details
+# 3) Submit a pull request
+
+{
+  :last_name => "Komlo",
+  :first_name => "Chelsea",
+  :project_name => "Coordinating Groups in Natural Disasters",
+  :project_description => "This application allows for group coordination and tracking individuals in the wake of natural disasters",
+  :technologies => ['CouchDB',],
+  :integrations => ['google maps']
+  :repo => 'https://github.com/chelseakomlo/disaster_relief',
+  :heroku_account => 'chelsea.komlo@gmail.com'
+}
