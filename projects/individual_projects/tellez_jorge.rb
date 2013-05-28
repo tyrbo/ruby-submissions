ff1851c02f4c8d8e6dbde31d0085ea10a80f9284
novohispano@gmail.com <novohispano@gmail.com>
2013-05-28 14:45:14 -0600

    BandMatch

---
+++ projects/individual_projects/tellez_jorge.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "Téllez",
+  :first_name => "Jorge",
+  :project_name => "BandMatch",
+  :project_description => "Find someone to go to a concert.",
+  :technologies => ['memcache'],
+  :integrations => ['Spotify', 'Facebook']
+  :repo => 'http://github.com/username/repo_name',
+  :heroku_account => 'novohispano@gmail.com'
+}
