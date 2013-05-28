e4bf3025b42b83fb8e7dd1bd3521c17d2f7eaf1e
Aimee Maher <aimee@geekycheeks.com>
2013-05-28 14:09:42 -0600

    added my project

---
+++ projects/individual_projects/maher_aimee.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "maher",
+  :first_name => "aimee",
+  :project_name => "Caution Tape",
+  :project_description => "When checking into a location on foursquare, view data from Data.gov arrest records/criminal statistics databases and media coverage from the Associated Press API for that location. ",
+  :technologies => ['riak'],
+  :integrations => ['dropbox', 'sendgrid']
+  :repo => 'http://github.com/jmejia/sparklr',
+  :heroku_account => 'joshua.mejia@gmail.com'
+}
