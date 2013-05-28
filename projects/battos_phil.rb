df1846b4886f8a1886811387cb31ab81ec7fc5f7
Phil Battos <philbattos@gmail.com>
2013-05-28 13:58:08 -0600

    added project outline

---
+++ projects/battos_phil.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "Battos",
+  :first_name => "Phil",
+  :project_name => "Cache Map",
+  :project_description => "The main purpose of this app will be to take search results related to clues in the Fenn Treasure Hunt, find associated place names from USGS geonames database, and map all related results using Google Maps API. The search results can be used by the user to find likely locations for the treasure. Aggregated search results can be analyzed by developers to study how users are interpreting the clues and where they are searching.",
+  :technologies => ['Raphael.js'],
+  :integrations => ['Google Maps', 'Data.gov']
+  :repo => 'http://github.com/username/repo_name',
+  :heroku_account => 'philbattos@gmail.com'
+}
