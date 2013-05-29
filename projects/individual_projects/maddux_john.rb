e21d1c6f0b50b71fe4d378e728de1040282db959
John Maddux <jemaddux@gmail.com>
2013-05-29 16:07:47 -0600

    John's project

---
+++ projects/individual_projects/maddux_john.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "Maddux",
+  :first_name => "John",
+  :project_name => "Crafty",
+  :project_description => "One paragraph about what the app will do any why",
+  :technologies => ['elasticsearch','scraping'],
+  :integrations => ['etsy', 'michaels.com']
+  :repo => 'http://github.com/jemaddux/crafty',
+  :heroku_account => 'jemaddux@gmail.com'
+}
