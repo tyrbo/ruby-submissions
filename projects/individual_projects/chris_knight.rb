88eae856428a3eb62bde814c5d37393802d07f9d
Christopher <ChrisKnight.mail@gmail.com>
2013-05-29 12:29:34 -0500

    Create chris_knight.rb

---
+++ projects/individual_projects/chris_knight.rb
@@ -0,0 +1,10 @@
+{
+  :last_name => "Knight",
+  :first_name => "Christopher",
+  :project_name => "Hacktavism",
+  :project_description => "An application where you can submit a link to an embeddable video relating to activism, and also a set of tags. This will take the submitted content and find charities and volunteer opportunities related to the video content's tags.",
+  :technologies => ['VolunteerMatch.org, Charitynavigator.org'],
+  :integrations => ['YouTube', 'Vimeo']
+  :repo => 'https://github.com/SerKnight/hactavism.git',
+  :heroku_account => 'chrisknight.mail@gmail.com'
+}
