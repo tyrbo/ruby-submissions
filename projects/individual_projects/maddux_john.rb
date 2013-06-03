589fac77c274be88ab3f9bbbe1c8c01e81dae94e
John Maddux <jemaddux@gmail.com>
2013-06-03 13:04:23 -0600

    Updated John Maddux project

--- projects/individual_projects/maddux_john.rb
+++ projects/individual_projects/maddux_john.rb
@@ -2,9 +2,9 @@
   :last_name => "Maddux",
   :first_name => "John",
   :project_name => "Crafty",
-  :project_description => "A site that showsprojects from Etsy and automatically shows off the craft materials that you need to buy for that project next to it. ",
-  :technologies => ['elasticsearch','scraping'],
-  :integrations => ['etsy', 'amazon.com', 'michaels.com']
+  :project_description => "A site that lets you take your favorites from Etsy and create a poll that you can invite your friends to vote on.",
+  :technologies => [],
+  :integrations => ['etsy','facebook']
   :repo => 'http://github.com/jemaddux/crafty',
   :heroku_account => 'jemaddux@gmail.com'
 }
