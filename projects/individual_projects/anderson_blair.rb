4ecebbc855864e5ab975466502b9b35b2ea67be1
Blair Anderson <blair81@gmail.com>
2013-06-03 10:59:22 -0600

    updated anderson_blair with new heroku requirements

--- projects/individual_projects/anderson_blair.rb
+++ projects/individual_projects/anderson_blair.rb
@@ -5,10 +5,14 @@
 {
   :last_name => "Anderson",
   :first_name => "Blair",
-  :project_name => "Twitter Gists",
-  :project_description => "People will login with twitter. Their public profiles will be visible at example.com/twitterusername. They'll be able to create twoots which are longform-tweets. It will use Ember.",
-  :technologies => ['EMBER'],
+  :project_name => "ShareMore",
+  :project_description => "ShareMore is a website that makes it easy for people to share their long-form thoughts through a simple interface or secure API. Users first authenticate with twitter, then they write articles on the website or POST an article body through our API. The article will be visible to people with the link, the summary and link will be republished on their twitter feed. The users public feed is visible at example.com/twitterusername.",
+  :technologies => ['App API','API Gem'],
   :integrations => ['twitter']
-  :repo => 'http://github.com/blairanderson/solo-project',
+  :repo => 'https://github.com/blairanderson/share-more',
   :heroku_account => 'blair81@gmail.com'
+  :needs_heroku_setup => true,
+  :needs_workers => true,
+  :needs_redis_to_go => true,
+  :other_needs => ["none"]
 }
