e085a2d596723554986749a8c8c06cb122700317
Danny Garcia <dannygarcia.me@gmail.com>
2013-06-05 20:05:25 -0500

    Update
    Updating app info

--- projects/individual_projects/garcia_danny.rb
+++ projects/individual_projects/garcia_danny.rb
@@ -1,10 +1,11 @@
 {
   :last_name => "Garcia",
   :first_name => "Danny",
-  :project_name => "EconoMapper",
-  :project_description => "EconoMapper provides a data visualization of Economic Activity in the U.S. Allowing users to get a visual representation of various economic indicators state by state and the country as a whole. With this information for example, you can draw correlations between GDP and energy consumption in the U.S. or conduct research on business activities in industries and communities during the past few years.",
+  :project_name => "BizStartr",
+  :project_description => "BizStartr integrates with one of the SBA's APIs to provide entrepreneurs with information about federal, state, and local licences and permits required to start a business in a certain state. BizStarts takes advantage of D3.js to provide a visualization of the United States, allowing users to easily choose a state and quickly get access to a myriad of information about starting a business in a certain state.",
   :technologies => ['D3'],
-  :integrations => ['data.gov']
-  :repo => 'TBA',
-  :heroku_account => 'dannygarcia.me@gmail.com'
+  :integrations => ['sba.gov']
+  :repo => 'https://github.com/danny-g/biz_startr',
+  :heroku_account => 'dannygarcia.me@gmail.com',
+  :url => 'http://bizstartr.herokuapp.com/'
 }
