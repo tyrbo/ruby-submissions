c9352c1f02f967dcf2a37bff21056b9110601c6f
Laura <lauramsteadman@gmail.com>
2013-06-06 08:21:11 -0500

    Update Steadman project
    do you need our site url's too? Mine is trekkr.herokuapp.com

--- projects/individual_projects/steadman_laura.rb
+++ projects/individual_projects/steadman_laura.rb
@@ -2,9 +2,9 @@
   :last_name => "Steadman",
   :first_name => "Laura",
   :project_name => "Trekkr",
-  :project_description => "Cartographic Exploration of Hiking Adventures, One Photo at a Time. This project will map Instagram photos associated with popular, long-distance hiking trails.",
+  :project_description => "Take a virtual hike along the Pacific Crest Trail, via geo-tagged Instagram photos.",
   :technologies => ['MongoDB'],
-  :integrations => ['Instagram', 'Google Maps (would like to try using Leaflet)']
-  :repo => 'http://github.com/username/repo_name',
+  :integrations => ['Instagram', 'Mapbox/Leaflet']
+  :repo => 'http://github.com/thesteady/trekkr',
   :heroku_account => 'lauramsteadman@gmail.com'
 }
