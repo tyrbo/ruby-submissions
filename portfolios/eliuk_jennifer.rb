cc2babdf41144e737a07d0b39b13de380a4456e9
Jennifer Eliuk <jen7maples@gmail.com>
2013-06-07 10:13:37 -0500

    Create eliuk_jennifer.rb
    'looking for' is listed twice, wasn't sure if that was a typo. I don't want to publish my actual resume online, is it okay to just point them to my site and they can request it? Thanks!

---
+++ portfolios/eliuk_jennifer.rb
@@ -0,0 +1,33 @@
+{
+  :last_name => "Eliuk",
+  :first_name => "Jennifer",
+  :email_address => "jen7maples@gmail.com",
+  :looking_for => "Apprenticeship with a development team that has high standards for quality code, testing and delivering great products.",
+  :best_at => ["Clear, concise and explicit code", 'Testing', 'Coordinating team'],
+  :looking_for => ["Apprenticeship"],
+  :locations => ["Colorado", "Vancouver, B.C.", "Seattle", "Other U.S. & Canada", "International"]
+  :code => {
+    :github => 'http://github.com/7maples',
+    :project_1 => {
+      :title => "Patient Intake"
+      :description => "My first individual Rails project ever, a program to replace paper intake forms at doctors' offices."
+      :url => 'http://patient-intake.herokuapp.com'
+      :what_to_look_for => ['Used hstore to collect form responses instead of many tables and columns', 'Integrated with the Twilio API.', 'Used jQuery to create search form that displays results without reloading the page.']
+    }
+    :project_2 => {
+      :title => "Photoline"
+      :description => "Group project to learn API integration."
+      :url => 'https://github.com/pnblackwell/feed_engine'
+      :what_to_look_for => ['Focused on signing in users with Twitter/Omniauth and sending automatic emails.']
+    }
+    :project_3 => {
+      :title => "e-Commerce Proficiency"
+      :description => "Group project to expand multi-tenant e-Commerce platform legacy code to allow platform admin to invoice stores for a cut of the profits."
+      :url => 'https://github.com/jmejia/daughter_of_store_engine'
+      :what_to_look_for => ['Helped create a service to handle invoices.']
+    }
+  },
+  :resume => "jennifereliuk.com",
+  :references => ['Mike Gehard, Pivotal Labs', 'Cory Flanigan, Groupon', 'Katrina Owen, Jumpstart Lab']
+  :other_notes => "I had no programming experience before gSchool except for one Railsbridge workshop. I now attend Railsbridge as a TA, and helping others learn to program is important to me. I hold both U.S. and Canadian passports and am happy to relocate."
+}
