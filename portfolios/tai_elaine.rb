ba2e9478efcb712e10d9e4e887a0c940ea083855
lalalainexd <lalalainexd@gmail.com>
2013-06-07 15:12:32 -0600

    Added Elaine's portfolio info :turtle:

---
+++ portfolios/tai_elaine.rb
@@ -0,0 +1,34 @@
+
+{
+  :last_name => "Tai",
+  :first_name => "Elaine",
+  :email_address => "elaine.tai96@gmail.com",
+  :looking_for => "I'm looking for a fullstack Ruby on Rails position at a company that values TDD, agile development, code quality, and pair programming.",
+  :best_at => ["Ruby on Rails", 'TDD', 'pairing'],
+  :looking_for => ["Fullstack Developer"],
+  :locations => ["Boulder/Denver", "International", "NYC", "Austin", "Washington DC"]
+  :code => {
+    :github => 'http://github.com/lalalainexd',
+    :project_1 => {
+      :title => "Walk Me Home"
+      :description => "Walk My Home is a texting application that focuses on helping people get home safely. Registered users can send a text to the service, indicating how long it will take them to go home. After the alotted time, if the user does not notify the service that they have arrived home safely, then the service automatically notifies the user's ICE contacts."
+      :url => 'http://github.com/lalalainexd/walk_me_home'
+      :what_to_look_for => ['Used resque for scheduling background workers', 'Separating the concept of a Trip and Trek for flexibility in future development']
+    }
+    :project_2 => {
+      :title => "yHate"
+      :description => "Yhate is a Rails application aggregating user generated stories and hate crime statistics. It uses hatolence, a gem for the hate_crimes api which provides hate crime data."
+      :url => 'http://github.com/lalalainexd/yhate'
+      :what_to_look_for => ['wrapper around hatolence gem to decouple yhate and hatolence gem', 'built an api', 'built a gem around the api that yhate uses to consume the api data']
+    }
+    :project_3 => {
+      :title => "Son of Store Engine"
+      :description => "Son of Store Engine is an extension of a single store ecommerce application expanded to support multi-tenancy."
+      :url => 'http://github.com/lalalainexd/son_of_store_engine'
+      :what_to_look_for => ['scoping routes by the store name', 'namespaced routes for admin controllers']
+    }
+  },
+  :resume => "https://docs.google.com/document/d/17x6P9mKC9Sp8ZaoOw-o2MTsY4LkrrU0-OUote1xhARI/pub",
+  :references => ['Austen Ito, Laura Steadman, Aimee Maher']
+  :other_notes => ""
+}
