4e8e2e50d34aedc539f01b8d847290ea2150faa8
Franklin Webber <franklin.webber@gmail.com>
2013-06-10 16:28:26 -0600

    Adding blair and chelsea

---
+++ portfolios/anderson_blair.rb
@@ -0,0 +1,34 @@
+{
+  :last_name => "Anderson",
+  :first_name => "Blair",
+  :image_url => "/images/students/gschool0/blair.jpg",
+  :email_address => "blair81@gmail.com",
+  :looking_for_description => "I don't _just_ want to join a company, I want to join a team. I want to work with a group of people that care about educating each-other and working towards a common goal. I'm most passionate about disrupting politics, healthcare, payments, and education but would be happy to work outside of these industries. I'm eager to pair with amazing programmers, and to continually become better.",
+  :best_at => ['Testing First', 'Writing Clean Code', 'Getting Things Done!'],
+  :looking_for => ["Continuous Learning", 'Building Products', "Having Fun"],
+  :locations => ["Boulder", "Denver", "San Francisco", "Portland", "New York"],
+  :code => {
+    :github => 'https://github.com/blairanderson',
+    :project_1 => {
+      :title => "ShareMore - A Twitter client without a character limit.",
+      :description => "ShareMore was fun because I had decided to tackle a simple problem but make sure it was fully completed. In 7-days I created a main application and API structure, a small client library to interact with the application, and experimented with an Ember application as a front-end.",
+      :url => 'https://github.com/blairanderson/share-more',
+      :what_to_look_for => ['HyperMedia API endpoint at /api/v1', 'The Simplicity of The Gem', 'Clean Design and Thorough Implementation']
+    },
+    :project_2 => {
+      :title => "RunTalk - a mobile chat system",
+      :description => "RunTalk is a mobile chat system. It uses services to break up the architecture into more manageable pieces.",
+      :url => 'http://github.com/blairanderson/runtalk',
+      :what_to_look_for => ['You get services!', 'and you get services!', 'and you get services!']
+    },
+    :project_3 => {
+      :title => "HaxHub - Integrating the APIs of Github, Pivotal Tracker, and Travis CI to serve as an all-in-one software project feed.",
+      :description => "This was a fun experience trying to create a single-page application that aggregated a external content in real-time. We integrated WebHooks as well as some basic jQuery/CoffeeScript polling for new data.",
+      :url => 'https://github.com/blairanderson/haxhub',
+      :what_to_look_for => ['Consuming data through webhook', 'Good extraction of data extraction through APIs', 'Clean Implementation of jQuery for polling']
+    }
+  },
+  :resume => "http://blairbuilds.herokuapp.com/resume.html",
+  :references => [''],
+  :other_notes => "I am Awesome. Find me on twitter @blairanderson"
+}
---
+++ portfolios/komlo_chelsea.rb
@@ -0,0 +1,34 @@
+{
+  :last_name => "Komlo",
+  :first_name => "Chelsea",
+  :image_url => "/images/students/gschool/chelsea.jpg",
+  :email_address => "chelsea.komlo@gmail.com",
+  :looking_for => "Working with a software development team with a great culture, appreciation for test-driven and agile development, pair programming, contributing to open source, and building cutting-edge products. My interests include developing new products and hard engineering problems.",
+  :best_at => ["Test Driven Development", 'Refactoring', 'Ruby on Rails'],
+  :looking_for => ["Forward-thinking culture", 'Development best practices', "Interesting engineering problems"],
+  :locations => ["New York", "Chicago", "San Francisco", "International"],
+  :code => {
+    :github => 'http://github.com/chelseakomlo',
+    :project_1 => {
+      :title => "Findscape",
+      :description => "An application that leveraged a client/API architecture to connect individuals and disaster relief shelters using relative location in the case of a natural or manmade disasters. This application interfaced with Twilio and Google Maps APIs, and implemented a dashboard to track locations.",
+      :url => 'https://github.com/chelseakomlo/findscape',
+      :what_to_look_for => ['Service Oriented Architecture', 'Flexible Code', 'Test Coverage']
+    },
+    :project_2 => {
+      :title => "Walk Me Home",
+      :description => "Text-based application that serves to ensure individuals walking home will have an emergency contact notified should something dangerous happen.",
+      :url => 'https://github.com/chelseakomlo/walk_me_home',
+      :what_to_look_for => ['Use of external API', 'High quality code', 'Test Coverage']
+    },
+    :project_3 => {
+      :title => "Daughter of Store Engine",
+      :description => "Multi-tenant store that mimics Etsy. Use of product collections and curated products.",
+      :url => 'https://github.com/chelseakomlo/daughter_of_store_engine',
+      :what_to_look_for => ['Use of Redis to track unique page views', 'Multi-Tenant Store', 'Legacy Project']
+    }
+  },
+  :resume => "https://www.dropbox.com/s/pphlcnvdk5lavh8/Chelsea%20Holland%20Komlo%20Resume%202013.pdf",
+  :references => ['Ben Johnson, Tony Mugavero, Katrina Owen'],
+  :other_notes => "I like solving hard problems that make people's lives better."
+}
