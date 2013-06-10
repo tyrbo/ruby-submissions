31534e75e6f17f58400ca03b07efbc988635f802
Franklin Webber <franklin.webber@gmail.com>
2013-06-10 16:28:52 -0600

    Update the template ruby file for submissions

--- portfolios/lastname_firstname.rb
+++ portfolios/lastname_firstname.rb
@@ -3,32 +3,33 @@
   :last_name => "Your Last Name",
   :first_name => "Your First Name",
   :email_address => "you@example.com",
-  :looking_for => "A short paragraph, in real sentences, about what kind of role you're looking for.",
+  :image_url => "/images/students/gschool0/firstname.jpg",
+  :looking_for_description => "A short paragraph, in real sentences, about what kind of role you're looking for.",
   :best_at => ["Up to three bullet points of what you're best at", 'Thing Two', 'Thing Three'],
   :looking_for => ["Up to three bullet points of what you're looking for", 'Bullet two', "Bullet three"],
-  :locations => ["1-5 short places", "p2", "p3", "p4", "p5"]
+  :locations => ["1-5 short places", "p2", "p3", "p4", "p5"],
   :code => {
     :github => 'http://github.com/yourusername',
     :project_1 => {
-      :title => "Title as you want it to appear"
-      :description => "1-3 sentences about why this is interesting"
-      :url => 'http://github.com/yourusername/yourproject'
+      :title => "Title as you want it to appear",
+      :description => "1-3 sentences about why this is interesting",
+      :url => 'http://github.com/yourusername/yourproject',
       :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
-    }
+    },
     :project_2 => {
-      :title => "Title as you want it to appear"
-      :description => "1-3 sentences about why this is interesting"
-      :url => 'http://github.com/yourusername/yourproject'
+      :title => "Title as you want it to appear",
+      :description => "1-3 sentences about why this is interesting",
+      :url => 'http://github.com/yourusername/yourproject',
       :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
-    }
+    },
     :project_3 => {
-      :title => "Title as you want it to appear"
-      :description => "1-3 sentences about why this is interesting"
-      :url => 'http://github.com/yourusername/yourproject'
+      :title => "Title as you want it to appear",
+      :description => "1-3 sentences about why this is interesting",
+      :url => 'http://github.com/yourusername/yourproject',
       :what_to_look_for => ['up to three bullets of things to highlight', 'b2', 'b3']
     }
   },
   :resume => "http://example.com/you/resume.pdf",
-  :references => ['up to three optional programming references, like your mentor']
+  :references => ['up to three optional programming references, like your mentor'],
   :other_notes => "Anything else someone needs to know?"
 }
