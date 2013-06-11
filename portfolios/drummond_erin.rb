f0b1db90588008aa7c22595f5b3678acae669671
erindrummond <e.b.drummond@gmail.com>
2013-06-10 21:46:31 -0600

    updated portfolio

--- portfolios/drummond_erin.rb
+++ portfolios/drummond_erin.rb
@@ -2,9 +2,9 @@
   :last_name => "Drummond",
   :first_name => "Erin",
   :email_address => "e.b.drummond@gmail.com",
-  :looking_for_description => "I'm looking for a company with a culture of mentoring new employees and enthusiastically encourages my growth and creativity in becoming a mature developer.  I would like to work for a company that thinks of its employees as more than just employees, but as part of the company family, and that encourages innovative new ideas from everyone.  I want to work in an environment where my co-workers are excited about the work they are doing, take pride in the code they write, have open communication practices, and celebrate team and individual successes.  I would like to find a place that has room for me to grow within the company, and offers positive and constructive feedback to help me become a strong developer.  Culturally speaking, spontaneous events like pub trivia nights on the history of the company or a beer and bacon party?  Yes, please - I'll even help you plan and execute it.",
+  :looking_for_description => "I'm looking for a company with a culture of mentoring new employees, and that thinks of its employees as more than just employees, but as part of the company family.  I love what I do, and I want to work in an environment where my co-workers are equally excited about the work they are doing, take pride in the code they write, have open communication practices, and celebrate team successes.  I'm excited about continuing to learn after gSchool, and I hope to work for a company that has a diverse set of problems to tackle that contribute to my growth as a developer.",
   :best_at => ["Ruby on Rails", "Persistent problem-solving", "Debugging"],
-  :looking_for => ["Mentoring", "Flexible pairing", "Advancement opportunities"],
+  :looking_for => ["Full stack development with a focus on the back end", "Flexible pairing", "Employee growth through mentoring"],
   :locations => ["Denver", "Portland", "Boulder", "Chicago", "International"],
   :code => {
     :github => 'http://github.com/ebdrummond',
@@ -16,18 +16,18 @@
     },
     :project_2 => {
       :title => "Feed Engine",
-      :description => "A travel blogging engine that allows users to create trips, invite friends, and have all their tweets, photos, and check-ins integrated during the trip duration.  Friends not on the trip can follow along by viewing their travel feed.",
+      :description => "A travel blogging engine that allows users to create trips, invite friends, and have all their tweets, photos, and check-ins integrated during the trip duration.  Friends not on the trip can follow along by viewing their travel feed.  I was responsible for implementing the Foursquare and Instagram APIs, creating trips, and the 'trip traveler/follower' functionality.",
       :url => 'http://github.com/ebdrummond/feed_engine',
       :what_to_look_for => ['APIs for Twitter, Instagram, and Foursquare', 'Clean, DRY code - user_show.rb a great example']
     },
     :project_3 => {
       :title => "Daughter of Store Engine",
-      :description => "Working with legacy code from two previous multi-tenant e-commerce apps, this site implements invoice generation and tracking for the platform administrator, and online payments for store owners.",
+      :description => "Working with legacy code from two previous multi-tenant e-commerce apps, this site implements invoice generation and tracking for the platform administrator, and online payments for store owners.  My contributions included invoice creation and debugging problems with dates and currency.",
       :url => 'http://github.com/ebdrummond/daughter_of_store_engine',
       :what_to_look_for => ['Using URL date parameters for invoice generation', 'online payments', 'AJAX information updates']
     }
   },
   :resume => "http://ebdrummond.com/resume.pdf",
   :references => ['Katrina Owen', 'Kareem Grant', 'Kyle Suss'],
-  :other_notes => ""
+  :other_notes => "I did not have a background in development when I joined gSchool, but I have fallen in love with coding the past five months.  Now that I have the skills, it's addicting to code my dreams into reality."
 }
