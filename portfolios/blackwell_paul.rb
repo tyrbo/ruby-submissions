7267253d53be70504e32ee7753e547de92bc2cd4
Paul <pnblackwell@gmail.com>
2013-06-10 22:41:35 -0600

    added portfolio file

---
+++ portfolios/blackwell_paul.rb
@@ -0,0 +1,28 @@
+{
+  :last_name => "Blackwell",
+  :first_name => "Paul",
+  :email_address => "pnblackwell@gmail.com",
+  :image_url => "/images/students/gschool0/paul.jpg",
+  :looking_for_description => "I'm looking for a company with a great mission and culture that also focuses toward running on the best code possible. I want to contribute as a full-stack developer and I would absolutely love to work at a company that has a strong mobile foothold.",
+  :best_at => ["Working in a team", 'Ruby on Rails'],
+  :looking_for => ["A startup", 'Bullet two', "Bullet three"],
+  :locations => ["New York", "DC", "San Francisco", "Austin", "Anywhere"],
+  :code => {
+    :github => 'http://github.com/pnblackwell',
+    :project_1 => {
+      :title => "Photoline Feed Engine",
+      :description => "This project integrates the Flickr API and the 500px API into an app that provides photo feeds.",
+      :url => 'https://github.com/pnblackwell/feed_engine',
+      :what_to_look_for => ['I focused on the front end for a lot of the project, especially the dashboard and home page', 'Implementation of user and session data', 'Paired for the API integrations as well']
+    },
+    :project_2 => {
+      :title => "Daughter of Store Engine",
+      :description => "This project was a class-wide redo of our attempt at successfully implementing e-commerce multitenancy. Our focuses were: testing, reliability and optimizing legacy code.  We were tasked with allowing store owners to customize stores so they didn't have to all look the same.",
+      :url => 'https://github.com/philbattos/daughter_of_store_engine',
+      :what_to_look_for => ['Did a lot of pairing with the backend dashboard which was the core of our functionality']
+    },
+  },
+  :resume => "http://www.linkedin.com/profile/view?id=74756408",
+  :references => ['Bryan Powell- Founder of Metabahn'],
+  :other_notes => "My hobbies include making music and writing."
+}
