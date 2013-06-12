fecc5620477d11c75b885b18d4fcc5c575f45f49
Shane Rogers <rogerssh@tcd.ie>
2013-06-11 19:45:59 -0600

    resume added

--- portfolios/rogers_shane.rb
+++ portfolios/rogers_shane.rb
@@ -27,7 +27,7 @@
       :what_to_look_for => ['Test Quality', 'Composed Methods', 'Morris/Raphael JS']
     }
   },
-  :resume => "TBD",
+  :resume => "http://shaneblog.herokuapp.com/shane_rogers_cv.pdf",
   :references => ['Franklin Webber', 'Katrina Owen','Cory Flanigan']
   :other_notes => "I am a non-US resident, with a one year work visa commencing mid-late September 2013 whom is eligible for a H-1B visa."
 }
