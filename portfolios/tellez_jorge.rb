7e11829b4459e9ae2f27b499630e1b19510122b2
novohispano@gmail.com <novohispano@gmail.com>
2013-06-13 22:36:25 -0600

    Updated link to CV, locations and looking for.

--- portfolios/tellez_jorge.rb
+++ portfolios/tellez_jorge.rb
@@ -4,16 +4,16 @@
   :first_name => "Jorge",
   :image_url => "/images/students/gschool0/jorge.jpg",
   :email_address => "novohispano@gmail.com",
-  :looking_for_description => "I want to build carefully crafted products.",
+  :looking_for_description => "I am looking for a place where I can build carefully crafted products. I am looking for great teams where I can grow and learn.",
   :best_at => ["Product Design", "Backend", "Frontend"],
   :looking_for => ["Product-Oriented company.", "Great teams.", "Outstanding quality."],
-  :locations => ["San Francisco, CA", "New York, NY"],
+  :locations => ["San Francisco, CA", "New York, NY", "Anywhere if the company is worth it"],
   :code => {
     :github => 'http://github.com/novohispano',
     :project_1 => {
       :title => "HaxHub",
       :description => "We built a team project management tool that integrates with Pivotal Tracker, Github and Travis. We use Github for authentication, fetch the data for the services and refresh them using AJAX.",
-      :url => 'https://github.com/blairand/haxhub',
+      :url => 'https://github.com/blairanderson/haxhub',
       :what_to_look_for => ['Integration with Github', 'Integration with Pivotal Tracker', 'Integration with Travis']
     },
     :project_2 => {
@@ -29,7 +29,7 @@
       :what_to_look_for => ['Resque workers', 'Stripe integration', 'Sendgrid integration']
     }
   },
-  :resume => "http://novohispanoblog.herokuapp.com/resume.html",
-  :references => ['Frank Webber, Katrina Owen, Mislav Marohnić'],
+  :resume => "https://www.dropbox.com/s/40uonvzruoj73ox/130610%20T%C3%A9llez%20-%20CV.pdf",
+  :references => ['Jeff Casimir, Frank Webber, Katrina Owen, Mislav Marohnić'],
   :other_notes => "I started two companies. I have Business Development, Communications and Product Development experience in the US and Latin America."
 }
