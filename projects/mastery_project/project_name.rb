a527bb907269c8a58271d06e6295d8898d5345db
Jeff Casimir <jeff@casimircreative.com>
2013-07-05 12:59:12 -0600

    Template for mastery project

---
+++ projects/mastery_project/project_name.rb
@@ -0,0 +1,8 @@
+{
+  :developers => ["First Last 1", "First Last 2"]
+  :project_name => "The Project Name",
+  :project_description => "One paragraph about what the app will do and why",
+  :backend_technologies => ['riak'],
+  :frontend_technologies => ['D3']
+  :repo => 'http://github.com/username/repo_name'
+}
