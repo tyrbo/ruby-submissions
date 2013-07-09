4e14db29461047d5baada9fa5048fad8bf84a9b4
John Maddux <jemaddux@gmail.com>
2013-07-08 19:35:21 -0600

    John's project

---
+++ projects/mastery_project/2nd_bookshelf.rb
@@ -0,0 +1,8 @@
+{
+  :developers => ["John Maddux"],
+  :project_name => "2nd Bookshelf",
+  :project_description => "I love books; I want more people to have access to more books, so I am building out a Netflix for books. I started 2ndbookshelf.com as a first iteration of this last weekend and I want to continue it. Many of the problems associated with running a service like this are non-technical but have technical solutions like inventory management, limiting the service area, and calculating the profitability of books. I have a list of features I would eventually like to implement but would certainly only get through the first few in the next 10 days: 1. Payment with Stripe or Paypal. 2. Allow admins to create books from within the app and allow rating and longer descriptions. 3. Admin tools to see which books are in demand and see what percantage of the time they are available (visualized with D3). 4. Service area to limit the area where people can signup for an account so we don't ship books to Alaska, etc. 5. Lots more stuff I wouldn't have time to get to I am sure.",
+  :backend_technologies => ['PayPal, Stripe'],
+  :frontend_technologies => ['D3'],
+  :repo => 'http://github.com/jemaddux/2ndbookcase'
+}
