0e45edb49d07ac1f4cb963ea7983c054997be207
Jeff Casimir <jeff@casimircreative.com>
2013-12-10 15:27:16 -0700

    Adding fourth meal submission file

---
+++ gschool-1/projects/fourth_meal.rb
@@ -0,0 +1,49 @@
+# Add your git repo URL and
+# one or more collaborator accounts for push access
+
+## Example
+#
+# :group_x => {:members       => ['Jeff Casimir', 'Katrina Owen'],
+#              :repo_url      => 'https://github.com/JumpstartLab/curriculum.git',
+#              :tracker_url   => 'https://www.pivotaltracker.com/s/projects/900000',
+#              :staging_url   => 'http://rushing-river.example.com',
+#              :collaborators => ['jeff@jumpstartlab.com', 'katrina@jumpstartlab.com']},
+
+def project
+  {
+    :group_1 => {:members       => [],
+                 :repo_url      => '',
+                 :tracker_url   => '',
+                 :staging_url   => '',
+                 :collaborators => []},
+    :group_2 => {:members       => [],
+                 :repo_url      => '',
+                 :tracker_url   => '',
+                 :staging_url   => '',
+                 :collaborators => []},
+    :group_3 => {:members       => [],
+                 :repo_url      => '',
+                 :tracker_url   => '',
+                 :staging_url   => '',
+                 :collaborators => []},
+    :group_4 => {:members       => [],
+                 :repo_url      => '',
+                 :tracker_url   => '',
+                 :staging_url   => '',
+                 :collaborators => []},
+    :group_5 => {:members       => [],
+                 :repo_url      => '',
+                 :tracker_url   => '',
+                 :staging_url   => '',
+                 :collaborators => []},
+    :group_6 => {:members       => [],
+                 :repo_url      => '',
+                 :tracker_url   => '',
+                 :staging_url   => '',
+                 :collaborators => []}
+  }
+end
+
+def config
+  {:abbreviation => 'feed'}
+end
