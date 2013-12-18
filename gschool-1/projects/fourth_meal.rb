ddfa330cdfb398078a27acc74abd270e3c98e0a8
Jeff Casimir <jeff@casimircreative.com>
2013-12-18 08:37:18 -0700

    Added spots for technical evaluation of fourth meal

--- gschool-1/projects/fourth_meal.rb
+++ gschool-1/projects/fourth_meal.rb
@@ -1,56 +1,120 @@
 # Add your git repo URL and
 # one or more collaborator accounts for push access
 
-## Example
+## Example  
 #
-# :group_x => {:members       => ['Jeff Casimir', 'Katrina Owen'],
-#              :repo_url      => 'https://github.com/JumpstartLab/curriculum.git',
-#              :tracker_url   => 'https://www.pivotaltracker.com/s/projects/900000',
-#              :staging_url   => 'http://rushing-river.example.com',
-#              :collaborators => ['jeff@jumpstartlab.com', 'katrina@jumpstartlab.com']},
+#  :group_x => {:members       => ['Jeff Casimir', 'Katrina Owen'],
+#               :repo_url      => 'https://github.com/JumpstartLab/curriculum.git',
+#               :tracker_url   => 'https://www.pivotaltracker.com/s/projects/900000,
+#               :staging_url   => 'http://rushing-river.example.com',
+#               :collaborators => ['jeff@jumpstartlab.com', 'katrina@jumpstartlab.com']},
 
 def project
   {
-    :group1 => {:members       => ['Kevin Powell', 'Nikhil Taneja', 'Quentin Tai'],
-                 :repo_url      => 'https://github.com/QuillyT/fourth_meal',
-                 :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968834',
-                 :staging_url   => 'http://orderup.herokuapp.com/',
-                 :collaborators => ['nikhil.a.taneja@gmail.com','kevin.m.powell04@gmail.com','quillyt@gmail.com']},
-    :group2 => {:members       => ['Katrina Engelsted', 'Luke Martinez', 'Nathaniel Watts'],
-                 :repo_url      => 'https://github.com/thewatts/bearlyhungry',
-                 :tracker_url   => 'https://www.pivotaltracker.com/s/projects/970514',
-                 :staging_url   => 'http://staging.bearlyhungry.com/',
-                 :collaborators => ['katrina@engelsted.co', 'lukemartinez@gmail.com','watts@nathanielwatts.com']},
-    :group3 => {:members       => ['Ben Lewis', 'Billy Griffin', 'Magan Adam'],
-                 :repo_url      => 'https://github.com/fluxusfrequency/fourth_meal',
-                 :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968664',
-                 :staging_url   => 'http://noshify.herokuapp.com/',
-                 :collaborators => ['bennlewis@gmail.com', 'navyosu@gmail.com','Adam.dev89@gmail.com']},
-    :group4 => {:members       => ['Antony Siegert', 'Bryana Knight', 'Jonah Moses'],
-                 :repo_url      => 'https://github.com/JonahMoses/fourth_meal',
-                 :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968650',
-                 :staging_url   => 'http://fourth-meal.herokuapp.com/',
-                 :collaborators => ['agsiegert@hotmail.com', 'brknig11@gmail.com', 'jonahkmoses@gmail.com']},
-    :group5 => {:members       => ['Ben Horne', 'Louisa Barrett', 'Will Mitchell'],
-                 :repo_url      => 'https://github.com/wvmitchell/fourth_meal',
-                 :tracker_url   => 'https://www.pivotaltracker.com/s/projects/969468',
-                 :staging_url   => 'http://retto-staging.herokuapp.com/',
-                 :collaborators => ['louisabarrett@gmail.com', 'benhorne44@gmail.com', 'wvmitchell@gmail.com']},
-    :group6 => {:members       => ['Darryl Pequeen', 'Lauren Spath Luhring', 'Rolen Le'],
-                 :repo_url      => 'https://github.com/dpequeen/Fourth_Meal',
-                 :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968636',
-                 :staging_url   => 'http://hunger-gains.herokuapp.com/',
-                 :collaborators => ['lespath@gmail.com', 'rolentle@gmail.com', 'pequickster@msn.com']},
-    :group7 => {:members       => ['Persa Zula', 'Romeeka Gayhart', 'Tyler Long'],
-                 :repo_url      => 'https://github.com/pzula/fourth_meal',
-                 :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968652',
-                 :staging_url   => 'craveyard-staging.herokuapp.com',
-                 :collaborators => ['persa@persazula.com','rrgayhart@gmail.com','tyler.stephen.long@gmail.com']},
-    :group8 => {:members       => ['Bree Thomas', 'Brian Winterling', 'George Hudson', 'Simon Taranto'],
-                 :repo_url      => 'https://github.com/bwinterling/fourthmeal',
-                 :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968674',
-                 :staging_url   => 'http://staging-fourthmeal.herokuapp.com/',
-                 :collaborators => ['bree@aha-method.com','bwinterling@yahoo.com','georgehudson78@gmail.com','simon.taranto@gmail.com']}
+    :group1 => {  :members       => ['Kevin Powell', 'Nikhil Taneja', 'Quentin Tai'],
+                  :repo_url      => 'https://github.com/QuillyT/fourth_meal',
+                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968834',
+                  :staging_url   => 'http://orderup.herokuapp.com/',
+                  :collaborators => ['nikhil.a.taneja@gmail.com','kevin.m.powell04@gmail.com','quillyt@gmail.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group2 => {  :members       => ['Katrina Engelsted', 'Luke Martinez', 'Nathaniel Watts'],
+                  :repo_url      => 'https://github.com/thewatts/bearlyhungry',
+                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/970514',
+                  :staging_url   => 'http://staging.bearlyhungry.com/',
+                  :collaborators => ['katrina@engelsted.co', 'lukemartinez@gmail.com','watts@nathanielwatts.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group3 => {  :members       => ['Ben Lewis', 'Billy Griffin', 'Magan Adam'],
+                  :repo_url      => 'https://github.com/fluxusfrequency/fourth_meal',
+                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968664',
+                  :staging_url   => 'http://noshify.herokuapp.com/',
+                  :collaborators => ['bennlewis@gmail.com', 'navyosu@gmail.com','Adam.dev89@gmail.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group4 => {  :members       => ['Antony Siegert', 'Bryana Knight', 'Jonah Moses'],
+                  :repo_url      => 'https://github.com/JonahMoses/fourth_meal',
+                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968650',
+                  :staging_url   => 'http://fourth-meal.herokuapp.com/',
+                  :collaborators => ['agsiegert@hotmail.com', 'brknig11@gmail.com', 'jonahkmoses@gmail.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group5 => {  :members       => ['Ben Horne', 'Louisa Barrett', 'Will Mitchell'],
+                  :repo_url      => 'https://github.com/wvmitchell/fourth_meal',
+                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/969468',
+                  :staging_url   => 'http://retto-staging.herokuapp.com/',
+                  :collaborators => ['louisabarrett@gmail.com', 'benhorne44@gmail.com', 'wvmitchell@gmail.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group6 => {  :members       => ['Darryl Pequeen', 'Lauren Spath Luhring', 'Rolen Le'],
+                  :repo_url      => 'https://github.com/dpequeen/Fourth_Meal',
+                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968636',
+                  :staging_url   => 'http://hunger-gains.herokuapp.com/',
+                  :collaborators => ['lespath@gmail.com', 'rolentle@gmail.com', 'pequickster@msn.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group7 => {  :members       => ['Persa Zula', 'Romeeka Gayhart', 'Tyler Long'],
+                  :repo_url      => 'https://github.com/pzula/fourth_meal',
+                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968652',
+                  :staging_url   => 'craveyard-staging.herokuapp.com',
+                  :collaborators => ['persa@persazula.com','rrgayhart@gmail.com','tyler.stephen.long@gmail.com'],
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                },
+    :group8 => {  :members       => ['Bree Thomas', 'Brian Winterling', 'George Hudson', 'Simon Taranto'],
+                  :repo_url      => 'https://github.com/bwinterling/fourthmeal',
+                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968674',
+                  :staging_url   => 'http://staging-fourthmeal.herokuapp.com/',
+                  :collaborators => ['bree@aha-method.com','bwinterling@yahoo.com','georgehudson78@gmail.com','simon.taranto@gmail.com',
+                  :technical_evaluation => {
+                    :modeling    => 0,
+                    :interface   => 0,
+                    :performance => 0,
+                    :process     => 0,
+                    :comments    => ""
+                  }
+                }
   }
 end
 
