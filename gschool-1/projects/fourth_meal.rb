a6f4497037722d38fa1bedf89fb1446ec026a8fc
Jeff Casimir <jeff@casimircreative.com>
2013-12-17 21:32:52 -0700

    Revising Heroku provisioning script for new class and data structure

--- gschool-1/projects/fourth_meal.rb
+++ gschool-1/projects/fourth_meal.rb
@@ -11,42 +11,42 @@
 
 def project
   {
-    :group_1 => {:members       => ['Kevin Powell', 'Nikhil Taneja', 'Quentin Tai'],
+    :group1 => {:members       => ['Kevin Powell', 'Nikhil Taneja', 'Quentin Tai'],
                  :repo_url      => 'https://github.com/QuillyT/fourth_meal',
                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968834',
                  :staging_url   => 'http://orderup.herokuapp.com/',
                  :collaborators => ['nikhil.a.taneja@gmail.com','kevin.m.powell04@gmail.com','quillyt@gmail.com']},
-    :group_2 => {:members       => ['Katrina Engelsted', 'Luke Martinez', 'Nathaniel Watts'],
+    :group2 => {:members       => ['Katrina Engelsted', 'Luke Martinez', 'Nathaniel Watts'],
                  :repo_url      => 'https://github.com/thewatts/bearlyhungry',
                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/970514',
                  :staging_url   => 'http://staging.bearlyhungry.com/',
                  :collaborators => ['katrina@engelsted.co', 'lukemartinez@gmail.com','watts@nathanielwatts.com']},
-    :group_3 => {:members       => ['Ben Lewis', 'Billy Griffin', 'Magan Adam'],
+    :group3 => {:members       => ['Ben Lewis', 'Billy Griffin', 'Magan Adam'],
                  :repo_url      => 'https://github.com/fluxusfrequency/fourth_meal',
                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968664',
                  :staging_url   => 'http://noshify.herokuapp.com/',
                  :collaborators => ['bennlewis@gmail.com', 'navyosu@gmail.com','Adam.dev89@gmail.com']},
-    :group_4 => {:members       => ['Antony Siegert', 'Bryana Knight', 'Jonah Moses'],
+    :group4 => {:members       => ['Antony Siegert', 'Bryana Knight', 'Jonah Moses'],
                  :repo_url      => 'https://github.com/JonahMoses/fourth_meal',
                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968650',
                  :staging_url   => 'http://fourth-meal.herokuapp.com/',
                  :collaborators => ['agsiegert@hotmail.com', 'brknig11@gmail.com', 'jonahkmoses@gmail.com']},
-    :group_5 => {:members       => ['Ben Horne', 'Louisa Barrett', 'Will Mitchell'],
+    :group5 => {:members       => ['Ben Horne', 'Louisa Barrett', 'Will Mitchell'],
                  :repo_url      => 'https://github.com/wvmitchell/fourth_meal',
                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/969468',
                  :staging_url   => 'http://retto-staging.herokuapp.com/',
                  :collaborators => ['louisabarrett@gmail.com', 'benhorne44@gmail.com', 'wvmitchell@gmail.com']},
-    :group_6 => {:members       => ['Darryl Pequeen', 'Lauren Spath Luhring', 'Rolen Le'],
+    :group6 => {:members       => ['Darryl Pequeen', 'Lauren Spath Luhring', 'Rolen Le'],
                  :repo_url      => 'https://github.com/dpequeen/Fourth_Meal',
                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968636',
                  :staging_url   => 'http://hunger-gains.herokuapp.com/',
                  :collaborators => ['lespath@gmail.com', 'rolentle@gmail.com', 'pequickster@msn.com']},
-    :group_7 => {:members       => ['Persa Zula', 'Romeeka Gayhart', 'Tyler Long'],
+    :group7 => {:members       => ['Persa Zula', 'Romeeka Gayhart', 'Tyler Long'],
                  :repo_url      => 'https://github.com/pzula/fourth_meal',
                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968652',
                  :staging_url   => 'craveyard-staging.herokuapp.com',
                  :collaborators => ['persa@persazula.com','rrgayhart@gmail.com','tyler.stephen.long@gmail.com']},
-    :group_8 => {:members       => ['Bree Thomas', 'Brian Winterling', 'George Hudson', 'Simon Taranto'],
+    :group8 => {:members       => ['Bree Thomas', 'Brian Winterling', 'George Hudson', 'Simon Taranto'],
                  :repo_url      => 'https://github.com/bwinterling/fourthmeal',
                  :tracker_url   => 'https://www.pivotaltracker.com/s/projects/968674',
                  :staging_url   => 'http://staging-fourthmeal.herokuapp.com/',
@@ -55,5 +55,5 @@ def project
 end
 
 def config
-  {:abbreviation => 'fourth_meal'}
+  {:abbreviation => 'fourth-meal'}
 end
--- scripts/provision_heroku_servers.rb
+++ scripts/provision_heroku_servers.rb
@@ -1,71 +1,73 @@
-require './projects/feed_engine'
+require './gschool-1/projects/fourth_meal'
 
-def setup
-  Dir.mkdir(temp_dir)
-  Dir.chdir(temp_dir)
-end
-
-def temp_dir
-  "#{config[:abbreviation]}_temp"
-end
-
-def clone_repository(repo, name)
-  `git clone #{repo} #{name}`
-end
-
-def create_app(name)
-  `heroku create #{config[:abbreviation]}-#{name}`
-end
-
-def setup_database
-  db_message = `heroku addons:add heroku-postgresql:basic`
-  db_name = db_message.scan(/Attached as (\S*)$/).first.first
-  `heroku pg:promote #{db_name}`
-end
-
-def deploy
-  `git push heroku master`
-  `heroku run rake db:migrate`
-end
-
-def start_processes
-  `heroku ps:scale web=1 worker=1`
-end
-
-def add_addons
-  `heroku addons:add redistogo`
-  `heroku addons:add newrelic:standard`
-end
-
-def add_collaborator(account)
-  `heroku sharing:add #{account}`
-end
-
-def teardown
-  `rm -rf #{temp_dir}`
-end
-
-def within_project_directory(name, &block)
-  Dir.chdir(name.to_s)
-  block.call
-  Dir.chdir('..')
-end
-
-def run
-  setup
-
-  project.each do |team_name, details|
-    clone_repository(details[:repo], team_name)
-    
-    within_project_directory(team_name) do
-      create_app(team_name)
-      setup_database
-      add_addons
-      deploy
-      start_processes
-      details[:collaborators].each{|account| add_collaborator(account)}
-    end
+class Provisioner
+  def setup
+    Dir.mkdir(temp_dir)
+    Dir.chdir(temp_dir)
   end
 
-  teardown
+  def temp_dir
+    "#{config[:abbreviation]}_temp"
+  end
+
+  def clone_repository(repo, name)
+    `git clone #{repo} #{name}`
+  end
+
+  def create_app(name)
+    `heroku create #{config[:abbreviation]}-#{name}`
+  end
+
+  def setup_database
+    db_message = `heroku addons:add heroku-postgresql:basic`
+    db_name = db_message.scan(/Attached as (\S*)$/).first.first
+    `heroku pg:promote #{db_name}`
+  end
+
+  def deploy
+    `git push heroku master`
+    `heroku run rake db:migrate`
+  end
+
+  def start_processes
+    `heroku ps:scale web=1 worker=1`
+  end
+
+  def add_addons
+    `heroku addons:add redistogo`
+    `heroku addons:add newrelic:stark`
+  end
+
+  def add_collaborator(account)
+    `heroku sharing:add #{account}`
+  end
+
+  def teardown
+    `rm -rf #{temp_dir}`
+  end
+
+  def within_project_directory(name, &block)
+    Dir.chdir(name.to_s)
+    block.call
+    Dir.chdir('..')
+  end
+
+  def run
+    setup
+
+    project.each do |team_name, details|
+      clone_repository(details[:repo_url], team_name)
+      
+      within_project_directory(team_name) do
+        create_app(team_name)
+        setup_database
+        add_addons
+        deploy
+        start_processes
+        details[:collaborators].each{|account| add_collaborator(account)}
+      end
+    end
+
+    teardown
+  end
 end
