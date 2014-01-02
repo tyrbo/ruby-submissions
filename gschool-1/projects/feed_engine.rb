17ed6aa16529d98a27eb223f014c1e58199716a6
Jeff Casimir <jeff@casimircreative.com>
2014-01-02 08:54:17 -0700

    Framework for FeedEngine group tracking

---
+++ gschool-1/projects/feed_engine.rb
@@ -0,0 +1,74 @@
+def project
+  {
+    :group1 => {  :project_name    => '',
+                  :members         => ['Jonah Moses', 'Magan Adam', 'Quentin Tai', 'Romeeka Gayhart'],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {},
+                  :learning_and_progress => {}
+                },
+    :group2 => {  :project_name    => '',
+                  :members         => ['Brian Winterling', 'Katrina Engelsted', 'Persa Zula', 'Rolen Le'],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {},
+                  :learning_and_progress => {}
+                },
+    :group3 => {  :project_name    => '',
+                  :members         => ['Kevin Powell', 'Nathaniel Watts', 'Simon Taranto', 'Tyler Long'],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {},
+                  :learning_and_progress => {}
+                },
+    :group4 => {  :project_name    => '',
+                  :members         => ['Billy Griffin', 'Bree Thomas', 'Lauren Spath Luhring', 'Luke Martinez'],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {},
+                  :learning_and_progress => {}
+                },
+    :group5 => {  :project_name    => '',
+                  :members         => ['Ben Horne', 'Ben Lewis', 'Bryana Knight', 'Darryl Pequeen'],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {},
+                  :learning_and_progress => {}
+                },
+    :group6 => {  :project_name    => '',
+                  :members         => ['Antony Siegert', 'George Hudson', 'Louisa Barrett', 'Nikhil Taneja', 'Will Mitchell'],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {},
+                  :learning_and_progress => {}
+                },
+  }
+end
+
+def config
+  {:abbreviation => 'feed-engine'}
+end
