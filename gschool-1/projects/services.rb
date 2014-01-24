b7520bd32ada3a53c355222f15d72d7a105a6f06
Jeff Casimir <jeff@casimircreative.com>
2014-01-24 13:09:21 -0700

    Outlining services submission page

---
+++ gschool-1/projects/services.rb
@@ -0,0 +1,93 @@
+def project
+  {
+    :group1 => {  :project_name    => '',
+                  :members         => [''],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {
+                    :apis => 0,
+                    :workers => 0,
+                    :tdd => 0,
+                    :processes => 0,
+                    :comments => ""
+                  },
+                  :learning_and_progress => {}
+                },
+    :group2 => {  :project_name    => '',
+                  :members         => [''],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {                    
+                    :apis => 0,
+                    :workers => 0,
+                    :tdd => 0,
+                    :processes => 0,
+                    :comments => ""
+                  },
+                  :learning_and_progress => {}
+                },
+    :group3 => {  :project_name    => '',
+                  :members         => [''],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {},
+                  :technical_evaluation  => {
+                    :apis => 0,
+                    :workers => 0,
+                    :tdd => 0,
+                    :processes => 0,
+                    :comments => ""
+                  },
+                  :learning_and_progress => {}
+                },
+    :group4 => {  :project_name    => '',
+                  :members         => [''],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {
+                    :deployment => 0,
+                    :features => 0,
+                    :interface => 0,
+                    :concept => 0,
+                    :comments => ""
+                  },
+                  :technical_evaluation  => {},
+                  :learning_and_progress => {}
+                },
+    :group5 => {  :project_name    => '',
+                  :members         => [''],
+                  :repo_url        => '',
+                  :tracker_url     => '',
+                  :staging_url     => '',
+                  :production_url  => '',
+                  :vps_owner_email => '',
+                  :client_satisfaction   => {
+                    :deployment => 0,
+                    :features => 0,
+                    :interface => 0,
+                    :concept => 0,
+                    :comments => ""
+                  },
+                  :technical_evaluation  => {},
+                  :learning_and_progress => {}
+                }
+  }
+end
+
+def config
+  {:abbreviation => 'services'}
+end
